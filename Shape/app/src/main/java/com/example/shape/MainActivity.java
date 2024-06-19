9a9390272f86dd4b4748d782bf5c8ba0fdeffef7
dnjeh <ehdnj.001@gmail.com>
2024-06-19 15:11:46 +0900

    c

--- Shape/app/src/main/java/com/example/shape/MainActivity.java
+++ Shape/app/src/main/java/com/example/shape/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.shape;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.shape.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    ActivityResultLauncher<Intent> launcher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.startImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!binding.radioButton.isChecked()&&!binding.radioButton2.isChecked()) {
+                    Toast.makeText(MainActivity.this, "찾고 싶은 모양을 클릭하세요", Toast.LENGTH_SHORT).show();
+                } //안댐
+                else {
+                    Intent intent = new Intent(MainActivity.this, SubActivity.class);
+
+                    intent.putExtra("shape", binding.radioButton.isChecked()?0:1);
+                    launcher.launch(intent);
+                }
+            }
+        });
+
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        //다시 돌아왔을 때 실행되는 코드
+                    }
+                }
+        );
     }
 }
--- Shape/app/src/main/java/com/example/shape/SubActivity.java
+++ Shape/app/src/main/java/com/example/shape/SubActivity.java
@@ -2,13 +2,93 @@ package com.example.shape;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class SubActivity extends AppCompatActivity {
+import com.example.shape.databinding.ActivitySubBinding;
+
+public class SubActivity extends AppCompatActivity implements View.OnClickListener {
+    ActivitySubBinding binding;
+    FrameLayout[] shapeFrameLayout = new FrameLayout[6];
+    ImageView[] shapeImage = new ImageView[6];
+    int[] selectShape = new int[6];
+    int countShape = 0;
+    ImageView checkImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
+        binding = ActivitySubBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        int shapeNum = intent.getIntExtra("shape", 0);
+        if(shapeNum == 0) {
+            binding.imageView.setImageResource(R.drawable.circle);
+        }
+        else {
+            binding.imageView.setImageResource(R.drawable.square);
+        }
+        shapeFrameLayout[0] = binding.frameLayout01;
+        shapeFrameLayout[1] = binding.frameLayout02;
+        shapeFrameLayout[2] = binding.frameLayout03;
+        shapeFrameLayout[3] = binding.frameLayout04;
+        shapeFrameLayout[4] = binding.frameLayout05;
+        shapeFrameLayout[5] = binding.frameLayout06;
+
+        shapeImage[0] = binding.shape01;
+        shapeImage[1] = binding.shape02;
+        shapeImage[2] = binding.shape03;
+        shapeImage[3] = binding.shape04;
+        shapeImage[4] = binding.shape05;
+        shapeImage[5] = binding.shape06;
+
+        for(int i=0;i<shapeImage.length;i++) {
+            shapeImage[i].setOnClickListener(this);
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.shape01) {
+            selectUpdate(0);
+        }
+        else if(view.getId() == R.id.shape02) {
+            selectUpdate(1);
+        }
+        else if(view.getId() == R.id.shape03) {
+            selectUpdate(2);
+        }
+        else if(view.getId() == R.id.shape04) {
+            selectUpdate(3);
+        }
+        else if(view.getId() == R.id.shape05) {
+            selectUpdate(4);
+        }
+        else if(view.getId() == R.id.shape06) {
+            selectUpdate(5);
+        }
+    }
+    private void selectUpdate(int i) {
+        if(selectShape[i]==0&&countShape>=2) Toast.makeText(SubActivity.this, "욕심쟁이", Toast.LENGTH_SHORT).show();
+        else if(selectShape[i]==0) {
+            selectShape[i]=1;
+            countShape++;
+            checkImage = new ImageView(this);
+            checkImage.setImageResource(R.drawable.ok_check);
+            shapeFrameLayout[i].addView(checkImage);
+        }
+        else {
+            selectShape[i]=0;
+            countShape--;
+            shapeFrameLayout[i].removeViewAt(1);
+
+        }
     }
 }
